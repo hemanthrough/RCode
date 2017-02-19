cc0b12b3c135662b18fa8199d7db631ccd0f8aa3
hemanth kumar babu <hemanthrough@gmail.com>
2017-02-19 17:15:21 +0100

    added resolution for finding n

--- Asst 5 OC.R
+++ Asst 5 OC.R
@@ -1,5 +1,5 @@
 SSimple = 29.52
-plotOC <- function(sampleSize= 8){
+plotOC <- function(sampleSize= 8,resolution =71){
   muCritical= qt(.95,2*(sampleSize-1)) * SSimple *sqrt(2)/sqrt(sampleSize)
   pts <- seq(-10,55,by = .1)
   delta <-  muCritical - pts
@@ -8,4 +8,13 @@ plotOC <- function(sampleSize= 8){
   print(betaValues)
   par(new=TRUE)
   plot(pts,betaValues,col= round(runif(1,1,24)))
+  
+  #code to find expected n
+  #alpha+bta
+  n <- (qt(.5,2*(sampleSize-1))+qt(.95,2*(sampleSize-1)))^2 * sSimple^2 *2/resolution^2
+  paste("n required with  50", n)
+  
+  n <- (qt(.1,2*(sampleSize-1))+qt(.05,2*(sampleSize-1)))^2 * sSimple^2 *2/resolution^2
+  paste("n required with  10", n)
+  
 }
