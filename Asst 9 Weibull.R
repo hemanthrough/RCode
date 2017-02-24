1ad233f963c70493de9eaf65147afb5ac6feb75f
hemanth kumar babu <hemanthrough@gmail.com>
2017-02-24 10:33:12 +0100

    wiennbull

--- Asst 9 Weibull.R
+++ Asst 9 Weibull.R
@@ -4,14 +4,14 @@ ranks <- seq(1,length(data),by=1)
 fRanks <- (ranks-.3)/(length(data)+.4)
 #ln of sorted data
 lnSortedDAta<-log(sortedDAta)
-lnLnofTime <- log(-log(1-fRanks))
+lnLnofRanks <- log(-log(1-fRanks))
 #you are doing lny(ranks)~lnx(data)
 #but when you do lnx~lny u get the output as in excel
-lmData<-lm(lnSortedDAta~lnLnofTime)
+lmData<-lm(lnSortedDAta~lnLnofRanks)
 summary(lmData)
 
-plot(lnSortedDAta~lnLnofTime)
-lines(lmData$fitted.values~lnLnofTime,col="red")
+plot(lnSortedDAta~lnLnofRanks)
+lines(lmData$fitted.values~lnLnofRanks,col="red")
 beta = 1/lmData$coefficients[2]
 #beta value
 beta
